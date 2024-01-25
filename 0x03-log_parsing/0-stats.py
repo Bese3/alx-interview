abe0d5be4db3ea9d2e58f02b41a974f3d74e1cda
besufikad <07yilmabese@gmail.com>
2024-01-25 22:45:55 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,6 @@ parsing the output of generator code
 """
 import sys
 
-from datetime import datetime
-
 
 if __name__ == '__main__':
     """
@@ -15,38 +13,11 @@ if __name__ == '__main__':
     the total file size and the count of each status code every 10 lines.
     """
 
-    def validate_ip(ip_ad: str) -> bool:
-        """
-        The function `validate_ip` takes a string representing an
-        IP address and returns True if it is a valid IP address and
-        False otherwise.
-        """
-        parts = ip_ad.split(".")
-
-        if len(parts) != 4:
-            return False
-
-        for part in parts:
-            if not isinstance(int(part), int):
-                return False
-
-            if int(part) < 0 or int(part) > 255:
-                return False
-
-        return True
-
-    def signal_handler(total_size, stat_dict):
-        """
-        The function `signal_handler` is a Python function
-        that handles signals, printing the file size and
-        resetting global variables before exiting.
-        """
-        print(f"File size: {total_size}")
-        # total_size = 0
-        for k, v in stat_dict.items():
-            if v != 0:
-                print(f"{k}: {v}")
-                # stat_dict[k] = 0
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(file_size))
+        for k, v in stats.items():
+            if v:
+                print("{}: {}".format(k, v))
 
     status_code = [200, 301, 400, 401, 403, 404, 405, 500]
     stat_dict = {k: 0 for k in status_code}
@@ -54,18 +25,8 @@ if __name__ == '__main__':
     i = 0
     try:
         for line in sys.stdin:
+            i += 1
             line = line.rstrip().split(" ")
-            try:
-                ip_val = validate_ip(line[0])
-                format_string = "%Y-%m-%d %H:%M:%S.%f"
-                line[2] = line[2] + ' ' + line[3]
-                line[2] = line[2].replace('[', '')
-                line[2] = line[2].replace(']', '')
-                parsed_datetime = datetime.strptime(line[2], format_string)
-                if not ip_val or not isinstance(parsed_datetime, datetime):
-                    raise ValueError("")
-            except Exception:
-                pass
             try:
                 status = int(line[-2])
                 if status in status_code:
@@ -76,12 +37,9 @@ if __name__ == '__main__':
                 total_size += int(line[-1])
             except BaseException:
                 pass
-            i += 1
-
             if i % 10 == 0:
-                signal_handler(total_size, stat_dict)
-                # signal_handler(total_size, stat_dict)
-        signal_handler(total_size, stat_dict)
+                print_stats(stat_dict, total_size)
+        print_stats(stat_dict, total_size)
     except KeyboardInterrupt:
-        signal_handler(total_size, stat_dict)
+        print_stats(stat_dict, total_size)
         raise
