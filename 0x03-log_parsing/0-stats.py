a81adc698c408c1cbf21aa3968b4f7546caa4365
besufikad <07yilmabese@gmail.com>
2024-01-25 23:00:04 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,40 +6,34 @@ import sys
 
 
 if __name__ == '__main__':
-    """
-    The main function reads log lines from standard input,
-    parses them, validates the IP address and datetime format,
-    counts the occurrences of different status codes, and prints
-    the total file size and the count of each status code every 10 lines.
-    """
+
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
     def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(file_size))
-        for k, v in stats.items():
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
             if v:
                 print("{}: {}".format(k, v))
 
-    status_code = [200, 301, 400, 401, 403, 404, 405, 500]
-    stat_dict = {k: 0 for k in status_code}
-    total_size = 0
-    i = 0
     try:
         for line in sys.stdin:
-            i += 1
-            line = line.rstrip().split(" ")
+            count += 1
+            data = line.split()
             try:
-                status = int(line[-2])
-                if status in status_code:
-                    stat_dict[status] += 1
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
             except BaseException:
                 pass
             try:
-                total_size += int(line[-1])
+                filesize += int(data[-1])
             except BaseException:
                 pass
-            if i % 10 == 0:
-                print_stats(stat_dict, total_size)
-        print_stats(stat_dict, total_size)
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_stats(stat_dict, total_size)
+        print_stats(stats, filesize)
         raise
